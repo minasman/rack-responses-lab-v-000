e99f5b69fd91b298ee73dd5ae78e82cc73f9141f
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-06-30 21:11:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     time = Time.now 
-    case Time.now.hour 
+    case time.hour 
       when < 12 
         resp.write "Good Morning!"
       end
